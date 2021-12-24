b37c161aa2291abde5d774d5109000fe9b3c12de
Buff4eyes <claudemokbel@gmail.com>
2021-12-24 12:03:18 -0800

    models/index.js initialization

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const User = require('./User.js')
+const Post = require('./Post.js')
+const Comment = require('./Comment.js')
+
+User.hasMany(Post, { foreignKey: 'uid' })
+Post.belongsTo(User, { foreignKey: 'uid' })
+User.hasMany(Comment, { foreignKey: 'uid' })
+Comment.belongsTo(User, { foreignKey: 'uid' })
+Post.hasMany(Comment, { foreignKey: 'uid' })
+Comment.belongsTo(Post, { foreignKey: 'uid' })
+
+module.exports = { User, Post, Comment }
