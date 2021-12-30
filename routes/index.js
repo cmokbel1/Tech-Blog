1440fea0e1978ffa2fb9ae7a78fbc109826b3ea0
Buff4eyes <claudemokbel@gmail.com>
2021-12-30 10:58:10 -0800

    changed route names to match comment name change

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 router.use('/api', require('./userRoutes.js'))
 router.use('/api', require('./postRoutes.js'))
-router.use('api', require('./commentRoutes.js'))
+router.use('api', require('./noteRoutes.js'))
 
 module.exports = router
