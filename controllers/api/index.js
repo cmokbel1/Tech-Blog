554b2682d428987ba6bd8be31847f2e5bb5766bd
Buff4eyes <claudemokbel@gmail.com>
2022-01-03 10:46:19 -0800

    syntax

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router()
 
 router.use('/user', require('../userRoutes.js'))
 router.use('/post', require('../postRoutes.js'))
-router.use('/note', require('./noteRoutes.js'))
+router.use('/note', require('../noteRoutes.js'))
 
 module.exports = router
