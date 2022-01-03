8d62d700a6460078e2dfb48b67acc3c2a50e1864
Buff4eyes <claudemokbel@gmail.com>
2022-01-02 22:03:01 -0800

    editted userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 const { User, Post, Note } = require('../models')
 const passport = require('passport')
 const jwt = require('jsonwebtoken')
+const { redirect } = require('express/lib/response')
 
 //register account
 router.post('/users/register', (req, res) => {
-  User.register(new User({ username: req.body.username}), req.body.password, err => {
+  User.register(new User({ username: req.body.username }), req.body.password, err => {
     if (err) { console.log(err) }
     res.redirect('/login')
   })
@@ -13,17 +14,28 @@ router.post('/users/register', (req, res) => {
 
 
 //login account
-router.post('/users/login', (req, res) => {
+router.post('/login', (req, res) => {
   User.authenticate()(req.body.username, req.body.password, (err, user) => {
+    
     if (err) { console.log(err) }
-    res.json( user? {
-      username: user.username,
-      token: jwt.sign({ id: user.id }, process.env.SECRET)
-    } : null)
+
+    res.session.save(() => {
+      req.session.userId = user.id;
+      req.session.username = username;
+      req.session.loggedIn = true;
+
+
+      res.json(user ? {
+        username: user.username,
+        token: jwt.sign({ id: user.id }, process.env.SECRET)
+      } : null)
+    })
+
   })
+
 })
 
 //retrieve user profile
-router.get('/users/profile', passport.authenticate('jwt'), (req,res) =>  res.json(req.user))
+router.get('/users/profile', passport.authenticate('jwt'), (req, res) => res.json(req.user))
 
 module.exports = router 
